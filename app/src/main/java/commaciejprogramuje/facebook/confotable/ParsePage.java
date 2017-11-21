c963bceeea538e368af27c54f4802828f785d289
maciejprogramuje <maciejsz1979@gmail.com>
2017-11-21 12:59:41 +0100

    ok

--- app/src/main/java/commaciejprogramuje/facebook/confotable/ParsePage.java
+++ app/src/main/java/commaciejprogramuje/facebook/confotable/ParsePage.java
@@ -10,6 +10,7 @@ import net.fortuna.ical4j.model.DateTime;
 import net.fortuna.ical4j.model.Period;
 import net.fortuna.ical4j.model.PeriodList;
 import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.PropertyList;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -35,125 +36,49 @@ public class ParsePage extends AsyncTask<String, Void, ArrayList<OneMeeting>> {
 
     @Override
     protected ArrayList<OneMeeting> doInBackground(String... strings) {
+        //https://github.com/ical4j/ical4j/wiki/Examples
         Log.w("UWAGA", "parsuję...");
 
         ArrayList<OneMeeting> tempArr = new ArrayList<>();
 
-        /*InputStream is = null;
-        try {
-            String tSummary = "";
-            String tStartDate = "";
-            String tEndDate = "";
+        Calendar today = Calendar.getInstance();
+        Calendar monthAfterToday = Calendar.getInstance();
+        monthAfterToday.add(Calendar.MONTH, 1);
 
-            is = new URL(INPUT_FILE_URL).openStream();
-            net.fortuna.ical4j.model.Calendar cal = new CalendarBuilder().build(is);
-
-            for (Object vevent : cal.getComponents()) {
-                Component component = (Component) vevent;
-                //Log.w("UWAGA", "component: " + component.getName());
-                if (component.getName().equals("VEVENT")) {
-                    for (Object o : component.getProperties()) {
-                        Property property = (Property) o;
-                        String name = property.getName();
-                        String value = property.getValue();
-
-                        //Log.w("UWAGA", "property: " + name + "=" + value);
-
-                        if (name.equals("SUMMARY")) {
-                            tSummary = value == null ? "Spotkanie" : value;
-                        } else if (name.equals("DTSTART")) {
-                            tStartDate = value;
-                        } else if (name.equals("DTEND")) {
-                            tEndDate = value;
-                        }
-                    }
-
-                    if(isAfterCurrentDateAndTime(tEndDate)) {
-                        tempArr.add(new OneMeeting(tSummary, tStartDate, tEndDate, ""));
-                    }
-                    Collections.sort(tempArr);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParserException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }*/
-
-
-// Reading the file and creating the calendar
-
-        // 20171205T081500/PT1H15M
-        // 20171120T100000/20171120T110000
-        //https://github.com/ical4j/ical4j/wiki/Examples
-
-        InputStream is = null;
-        DateTime from = null;
-        DateTime to = null;
         Period period = null;
         net.fortuna.ical4j.model.Calendar cal = null;
+        String tSummary = "";
+        String tStartDate = "";
+        String tEndDate = "";
         try {
-            is = new URL(INPUT_FILE_URL).openStream();
+            InputStream is = new URL(INPUT_FILE_URL).openStream();
             cal = new CalendarBuilder().build(is);
-            from = new DateTime("20171120T000000Z");
-            to = new DateTime("20171212T070000Z");
-            period = new Period(from, to);
+            period = new Period(new DateTime(today.getTime()), new DateTime(monthAfterToday.getTime()));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParserException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
             e.printStackTrace();
         }
 
-// For each VEVENT in the ICS
         for (Object o : cal.getComponents("VEVENT")) {
             Component c = (Component)o;
             PeriodList list = c.calculateRecurrenceSet(period);
 
             for (Object po : list) {
-                String s = ((Period)po).getStart().toString();
-                String e = ((Period)po).getEnd().toString();
-                Log.w("UWAGA", "s: "+s+", e: "+e);
+                PropertyList properties = c.getProperties();
+                tSummary = properties.getProperty("SUMMARY").getValue();
+                tStartDate = ((Period)po).getStart().toString();
+                tEndDate = ((Period)po).getEnd().toString();
+
+                //Log.w("UWAGA", "sum: "+tSummary+", s: "+tStartDate+", e: "+tEndDate);
+                tempArr.add(new OneMeeting(tSummary, tStartDate, tEndDate, ""));
             }
         }
 
         Log.w("UWAGA", "koniec parsowania");
 
+        Collections.sort(tempArr);
         return tempArr;
-    }
-
-    private boolean isAfterCurrentDateAndTime(String s) {
-        int tYear = Integer.valueOf(s.substring(0, 4));
-        int tMonth = Integer.valueOf(s.substring(4, 6));
-        int tDay = Integer.valueOf(s.substring(6, 8));
-        int tHour = 0;
-        int tMinutes = 0;
-        if (s.length() > 9) {
-            tHour = Integer.valueOf(s.substring(9, 11));
-            tMinutes = Integer.valueOf(s.substring(11, 13));
-        }
-
-        Calendar actualCalendar = Calendar.getInstance();
-
-        if (tYear > actualCalendar.get(Calendar.YEAR)) {
-            return true;
-        } else if (tYear == actualCalendar.get(Calendar.YEAR) && tMonth > (actualCalendar.get(Calendar.MONTH) + 1)) {
-            return true;
-        } else if (tYear == actualCalendar.get(Calendar.YEAR) && tMonth == (actualCalendar.get(Calendar.MONTH) + 1) && tDay > actualCalendar.get(Calendar.DAY_OF_MONTH)) {
-            return true;
-        } else if (tYear == actualCalendar.get(Calendar.YEAR) && tMonth == (actualCalendar.get(Calendar.MONTH) + 1) && tDay == actualCalendar.get(Calendar.DAY_OF_MONTH) && tHour > actualCalendar.get(Calendar.HOUR_OF_DAY)) {
-            return true;
-        } else if (tYear == actualCalendar.get(Calendar.YEAR) && tMonth == (actualCalendar.get(Calendar.MONTH) + 1) && tDay == actualCalendar.get(Calendar.DAY_OF_MONTH) && tHour == actualCalendar.get(Calendar.HOUR_OF_DAY) && tMinutes > actualCalendar.get(Calendar.MINUTE)) {
-            return true;
-        }
-        return false;
     }
 
     @Override
